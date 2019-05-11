b302b4f308ddd567789dfab6a0d699facc76d0ac
tria1999 <dai18085@uom.edu.gr>
2019-05-11 22:30:58 +0300

    Συνεχεια του προηγουμενου...

--- UniHotelProject/src/User.java
+++ UniHotelProject/src/User.java
@@ -4,6 +4,10 @@ public class User {
 	
 	private String userName = "";
 	private String passCode = "";
+	private String name = "";
+	private String surname = "";
+	private int age;
+	private String email = "";
 	private ArrayList<Hotel> hotelList = new ArrayList<>();
 
 	public void PushHotelListInUser(Hotel h) {
